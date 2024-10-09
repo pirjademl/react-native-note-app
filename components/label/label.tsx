6ebb63de12f6b44913fba64db46e43abf80cbbd1
magdump <pirjademl7@gmail.com>
2024-10-09 18:02:16 +0530

    chore: remove unwanted imports and code indentation

--- components/label/label.tsx
+++ components/label/label.tsx
@@ -1,20 +1,20 @@
 import { FC } from "react";
 import { View, Text } from "react-native";
 interface LabelProps {
-  title: string;
-  containerStyle: string;
-  labelStyle: string;
+    title: string;
+    containerStyle: string;
+    labelStyle: string;
 }
 export const Label: FC<LabelProps> = ({
-  title,
-  containerStyle,
-  labelStyle,
+    title,
+    containerStyle,
+    labelStyle,
 }) => {
-  return (
-    <View className={`mt-2 font-semibold  ${containerStyle}`}>
-      <Text className={`mt-2 font-semibold  text-base ${labelStyle}`}>
-        {title}
-      </Text>
-    </View>
-  );
+    return (
+        <View className={`mt-2 font-semibold  ${containerStyle}`}>
+            <Text className={`mt-2 font-semibold  text-base ${labelStyle}`}>
+                {title}
+            </Text>
+        </View>
+    );
 };
--- components/note-card/note-card.tsx
+++ components/note-card/note-card.tsx
@@ -4,38 +4,37 @@ import { Button } from "../button/button";
 import { useNoteStore } from "../../app/store/store";
 
 interface NoteCard {
-  note: string;
-  status: string;
-  containerStyles: string;
-  noteStyle: string;
-  statusStyle: string;
+    note: string;
+    status: string;
+    containerStyles: string;
+    noteStyle: string;
+    statusStyle: string;
 }
 
 export const NoteCard: FC<NoteCard> = ({
-  containerStyles,
-  statusStyle,
-  status,
-  note,
-  noteStyle,
+    containerStyles,
+    status,
+    note,
+    noteStyle,
 }) => {
-  const removeNote = useNoteStore().removeNote;
-  return (
-    <View className={`border border-border mt-3  p-3 ${containerStyles}`}>
-      <Text className={`text-base p-2 ${noteStyle}`}>{note}</Text>
-      <View className=" flex justify-between flex-row">
-        <Text
-          className={`w-[35%] px-3 py-1 text-center text-white ${status === "not started" ? "bg-red-500" : "bg-green-400"}`}
-        >
-          {status}
-        </Text>
+    const removeNote = useNoteStore().removeNote;
+    return (
+        <View className={`border border-border mt-3  p-3 ${containerStyles}`}>
+            <Text className={`text-base p-2 ${noteStyle}`}>{note}</Text>
+            <View className=" flex justify-between flex-row">
+                <Text
+                    className={`w-[35%] px-3 py-1 text-center text-white ${status === "not started" ? "bg-red-500" : "bg-green-400"}`}
+                >
+                    {status}
+                </Text>
 
-        <Button
-          title="delete note"
-          textStyle="text-white text-center"
-          containerStyles={`w-[35%] px-3 py-2 text-center text-white `}
-          onClickHandler={() => removeNote({ note, status })}
-        />
-      </View>
-    </View>
-  );
+                <Button
+                    title="delete note"
+                    textStyle="text-white text-center"
+                    containerStyles={`w-[35%] px-3 py-2 text-center text-white `}
+                    onClickHandler={() => removeNote({ note, status })}
+                />
+            </View>
+        </View>
+    );
 };
