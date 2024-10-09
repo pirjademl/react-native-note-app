efe5bf16a940e326f62422dd8b3905bc63d6df6a
magdump <pirjademl7@gmail.com>
2024-10-09 17:58:56 +0530

    chore: remove unwanted imports

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,57 +1,55 @@
-import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import { TabIcon } from "../../components/icon/icon";
 import { icons } from "../../constants";
 
 const TabsLayout = () => {
-  return (
-    <>
-      <Tabs
-        screenOptions={{
-          tabBarShowLabel: false,
-          tabBarActiveTintColor: "#031D44",
-          tabBarInactiveTintColor: "#D8E1FF",
-          tabBarStyle: {
-            backgroundColor: "#1E3888",
-            borderTopWidth: 1,
-            borderTopColor: "#232533",
-          },
-        }}
-      >
-        <Tabs.Screen
-          name="create-note"
-          options={{
-            headerShown: false,
-            title: "Create",
-            tabBarIcon: ({ color, focused }) => (
-              <TabIcon
-                icon={icons.home}
-                color={color}
-                name="Home"
-                focused={focused}
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="view"
-          options={{
-            headerShown: false,
-            title: "View",
-            tabBarIcon: ({ color, focused }) => (
-              <TabIcon
-                icon={icons.eye}
-                color={color}
-                name="View"
-                focused={focused}
-              />
-            ),
-          }}
-        />
-      </Tabs>
-    </>
-  );
+    return (
+        <>
+            <Tabs
+                screenOptions={{
+                    tabBarShowLabel: false,
+                    tabBarActiveTintColor: "#031D44",
+                    tabBarInactiveTintColor: "#D8E1FF",
+                    tabBarStyle: {
+                        backgroundColor: "#1E3888",
+                        borderTopWidth: 1,
+                    },
+                }}
+            >
+                <Tabs.Screen
+                    name="create-note"
+                    options={{
+                        headerShown: false,
+                        title: "Create",
+                        tabBarIcon: ({ color, focused }) => (
+                            <TabIcon
+                                icon={icons.home}
+                                color={color}
+                                name="Home"
+                                focused={focused}
+                            />
+                        ),
+                    }}
+                />
+                <Tabs.Screen
+                    name="view"
+                    options={{
+                        headerShown: false,
+                        title: "View",
+                        tabBarIcon: ({ color, focused }) => (
+                            <TabIcon
+                                icon={icons.eye}
+                                color={color}
+                                name="View"
+                                focused={focused}
+                            />
+                        ),
+                    }}
+                />
+            </Tabs>
+        </>
+    );
 };
 
 export default TabsLayout;
--- app/(tabs)/create-note.tsx
+++ app/(tabs)/create-note.tsx
@@ -1,83 +1,88 @@
-import { FC, useState } from "react";
-import {
-  View,
-  Text,
-  SafeAreaView,
-  ToastAndroid,
-  Alert,
-  Modal,
-} from "react-native";
+import { useState } from "react";
+import { View, Text, SafeAreaView, ToastAndroid } from "react-native";
 import { Input } from "../../components/input/input";
 import { Label } from "../../components/label/label";
 import { Button } from "../../components/button/button";
 import { Picker } from "@react-native-picker/picker";
 import { Select } from "../../components/picker/picker";
 import { useNoteStore } from "../store/store";
+import { Inote } from "../../types/types";
 
-export interface Inote {
-  note: string;
-  status: string;
-}
 const CreateNote = () => {
-  const addNote = useNoteStore((state) => state.addNote);
+    const addNote = useNoteStore((state) => state.addNote);
 
-  const handleCreate = () => {
-    if (!note.note) {
-      setisNoteEmpty(!isnoteEmpty);
-      return;
-    }
-    addNote(note);
-    ToastAndroid.showWithGravityAndOffset(
-      "A fucking note was created",
-      ToastAndroid.LONG,
-      ToastAndroid.BOTTOM,
-      25,
-      50,
+    const handleCreate = () => {
+        if (!note.note) {
+            setisNoteEmpty(!isnoteEmpty);
+            return;
+        }
+        addNote(note);
+        ToastAndroid.showWithGravityAndOffset(
+            "A fucking note was created",
+            ToastAndroid.LONG,
+            ToastAndroid.BOTTOM,
+            25,
+            50,
+        );
+    };
+    const [selectedstate, setSelectedState] = useState("started");
+    const [note, setNote] = useState<Inote>({
+        note: "",
+        status: selectedstate,
+    });
+    const [isnoteEmpty, setisNoteEmpty] = useState<boolean>(false);
+    return (
+        <SafeAreaView className=" p-3 flex flex-col  justify-center h-full w-full bg-[#DDD8C4] ">
+            <View className="">
+                <Label
+                    title="create your note"
+                    containerStyle="m-3"
+                    labelStyle=" "
+                />
+                <Input
+                    inputStyle="border border-border p-2 rounded-md text-charcoal  text-base"
+                    placeholderText="Create a web3 wallet"
+                    containerStyle=""
+                    value={note.note}
+                    onInputChange={(text: string) =>
+                        setNote({ ...note, note: text })
+                    }
+                />
+
+                <Label
+                    title="status of the note"
+                    containerStyle="m-3"
+                    labelStyle=" "
+                />
+                <Select
+                    className="border border-border bg-red-500"
+                    selectedValue={selectedstate}
+                    onValueChange={(itemvalue, itemIndex) => (
+                        setSelectedState(itemvalue),
+                        setNote({ ...note, status: selectedstate })
+                    )}
+                    containerStyles="border border-border"
+                >
+                    <Picker.Item label="started" value="started" />
+                    <Picker.Item label="not started " value="not started" />
+                    <Picker.Item label="in progress" value="in progress" />
+                </Select>
+
+                <Button
+                    title="create"
+                    textStyle="text-center text-lg text-white"
+                    containerStyles="mt-4"
+                    onClickHandler={handleCreate}
+                />
+            </View>
+            {isnoteEmpty && (
+                <View className="p-3">
+                    <Text className="text-base text-red-600">
+                        Note text is empty!
+                    </Text>
+                </View>
+            )}
+        </SafeAreaView>
     );
-  };
-  const [selectedstate, setSelectedState] = useState("started");
-  const [note, setNote] = useState<Inote>({ note: "", status: selectedstate });
-  const [isnoteEmpty, setisNoteEmpty] = useState<boolean>(false);
-  return (
-    <SafeAreaView className=" p-3 flex flex-col  justify-center h-full w-full bg-[#DDD8C4] ">
-      <View className="">
-        <Label title="create your note" containerStyle="m-3" labelStyle=" " />
-        <Input
-          inputStyle="border border-border p-2 rounded-md text-charcoal  text-base"
-          placeholderText="Create a web3 wallet"
-          containerStyle=""
-          value={note.note}
-          onInputChange={(text: string) => setNote({ ...note, note: text })}
-        />
-
-        <Label title="status of the note" containerStyle="m-3" labelStyle=" " />
-        <Select
-          className="border border-border bg-red-500"
-          selectedValue={selectedstate}
-          onValueChange={(itemvalue, itemIndex) => (
-            setSelectedState(itemvalue),
-            setNote({ ...note, status: selectedstate })
-          )}
-          containerStyles="border border-border"
-        >
-          <Picker.Item label="started" value="started" />
-          <Picker.Item label="not started " value="not started" />
-          <Picker.Item label="in progress" value="in progress" />
-        </Select>
-
-        <Button
-          title="create"
-          textStyle="text-center text-lg text-white"
-          containerStyles="mt-4"
-          onClickHandler={handleCreate}
-        />
-      </View>
-      {isnoteEmpty && (
-        <View className="p-3">
-          <Text className="text-base text-red-600">Note text is empty!</Text>
-        </View>
-      )}
-    </SafeAreaView>
-  );
 };
 export default CreateNote;
